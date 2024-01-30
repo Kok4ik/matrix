9156b51ad870162a9c2cb5489d34dba31486cc05
Kok4ik <153661513+Kok4ik@users.noreply.github.com>
2024-01-30 21:23:33 +0300

    Update PortfolioPage.jsx

--- src/pages/PortfolioPage/PortfolioPage.jsx
+++ src/pages/PortfolioPage/PortfolioPage.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import cl from './PortfolioPage.module.css'
+import { PortfolioCard } from './card/PortfolioCard'
+const arr = [
+  {img: 'https://static-cse.canva.com/blob/847132/paulskorupskas7KLaxLbSXAunsplash2.jpg', btn1: '', btn2: '', btn3: '', btn4: ''},
+  {img: '', btn1: '', btn2: '', btn3: '', btn4: ''},
+  {img: '', btn1: '', btn2: '', btn3: '', btn4: ''},
+  {img: '', btn1: '', btn2: '', btn3: '', btn4: ''},
+  {img: '', btn1: '', btn2: '', btn3: '', btn4: ''},
+  {img: '', btn1: '', btn2: '', btn3: '', btn4: ''},
+  {img: '', btn1: '', btn2: '', btn3: '', btn4: ''},
+]
+
 
 function PortfolioPage() {
   return (
-    <div>Портфолио</div>
+    <div className='page'>
+      <div className={cl.title_page}>
+        <h1>Заголовок</h1>
+      </div>
+      <div className={cl.bank}>
+        {arr.map(card => 
+          <PortfolioCard {...card}/>
+        )}
+      </div>
+    </div>
   )
 }
 
